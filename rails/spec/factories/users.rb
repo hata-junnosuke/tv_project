e4c1ae8792f7d02420ffb7d180a8cf6a0f9039ac
hata-junnosuke <jh19911209@icloud.com>
2024-06-24 15:51:39 +0900

    [Rails]rspecの設定追加（factory_bot,faker）

---
+++ rails/spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  # TODO Googleログインように修正が必要
+  factory :user do
+    name { Faker::Name.name }
+    sequence(:email) {|n| "#{n}_" + Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 10) }
+    confirmed_at { Time.current }
+  end
+end
---
+++ rails/spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  # TODO Googleログインように修正が必要
+  context "factoryのデフォルト設定に従った場合" do
+    let(:user) { create(:user) }
+
+    it "認証済みの user レコードを正常に新規作成できる" do
+      expect(user).to be_valid
+      expect(user).to be_confirmed
+    end
+  end
+end
